1f46cd4a8a55386e8ae7b3d699d3b7c885daf89e
QuentinLem <quentin.lemonnier.info@gmail.com>
2017-01-06 22:07:29 +0100

    connexion client to server (py3)

--- cmdClient.py
+++ cmdClient.py
@@ -1,9 +1,9 @@
 # CMD Interface - Client version
 # Gestion des retours de commandes
 def cmd_getstate(grid):
-    
     # TODO : traitement retour de getState -> mise a jour de la grille locale
-
+    client.send(b"GETSTATE")
 
 def cmd_getscore(score):
     # TODO : traitement retour de getScore
+    client.send(b"GETSCORE")
--- mainClient.py
+++ mainClient.py
@@ -18,29 +18,26 @@ def prompt():
 # TODO 2 : Mettre dans un fichier a part - mainClient.py ? - et implementer les fonctions de traitement de retours de
 # commandes appellees dans cmdClient.py
 def main():
-    if len(sys.argv) < 3:
-        print 'Usage : python telnet.py hostname port'
+    if len(sys.argv) < 2:
+        print('Usage : python3 mainClient.py hostname')
         sys.exit()
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
+    host = '' #sys.argv[1]
+    port = 7777
+    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
     # connect to remote host
     try:
         s.connect((host, port))
     except:
-        print 'Unable to connect'
+        print('Unable to connect')
         sys.exit()
 
-    print 'Connected to remote host. Start sending messages'
+    print('Connected to remote host. Start sending messages')
     prompt()
 
     while 1:
         socket_list = [sys.stdin, s]
-
         # Get the list sockets which are readable
         read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
 
@@ -49,17 +46,16 @@ def main():
             if sock == s:
                 data = sock.recv(4096)
                 if not data:
-                    print '\nDisconnected from chat server'
+                    print('\nDisconnected from chat server')
                     sys.exit()
                 else:
-                    # print data
-                    sys.stdout.write(data)
+                    print(repr(data))
                     prompt()
 
             # user entered a message
             else:
                 msg = sys.stdin.readline()
-                s.send(msg)
+                s.send(msg.encode())
                 prompt()
     pass
 
